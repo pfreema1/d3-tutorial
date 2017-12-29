80bda2b4cadd9f94555955d82692150ba5c43174
Paul <pfreema1@gmail.com>
2017-12-29 11:29:09 -0500

    meow

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -51,119 +51,17 @@ var data = [
 
 //   END DATA
 
-console.log(data);
-
-var bubbleChart = function() {
-  var width = 600,
-    height = 400;
-
-  function chart(selection) {
-    //you gonna get here
-  }
-
-  chart.width = function(value) {
-    if (!arguments.length) {
-      return width;
-    }
-    width = value;
-
-    return chart;
-  };
-
-  chart.height = function(value) {
-    if (!arguments.length) {
-      return height;
-    }
-    height = value;
-
-    return chart;
-  };
-
-  return chart;
-};
-
-var chart = bubbleChart();
-chart.width(600).height(400);
-// console.log(chart.width()); //returns 600
-
-d3
-  .select("#chart")
-  .data(data)
-  .call(chart);
-
-var div = d3
+var canvas = d3
   .select("body")
-  .selectAll("div") //here you are saying, "hey d3, each data elemtn of the array that comes next will be bound to a div"
-  .data(data)
-  .enter()
-  .append("div");
+  .append("svg")
+  .attr("width", 500)
+  .attr("height", 500);
 
-var simulation = d3
-  .forceSimulation(data)
-  .force("charge", d3.forceManyBody().strength([-50]))
-  .force("x", d3.forceX())
-  .force("y", d3.forceY());
-// .on("tick", ticked);
+//path generator!
+var diagonal = d3.svg.diagonal();
 
-var scaleRadius = d3
-  .scaleLinear()
-  .domain([
-    d3.min(data, function(d) {
-      return 0;
-      // return +d.pomodoros;
-    }),
-    d3.max(data, function(d) {
-      return +d.pomodoros;
-    })
-  ])
-  .range([5, 18]);
-
-var colorCircles = d3.scaleOrdinal(d3.schemeCategory10);
-
-var tooltip = selection
-  .append("div")
-  .style("position", "absolute")
-  .style("visibility", "hidden")
-  .style("color", "white")
-  .style("padding", "8px")
-  .style("background-color", "#626D71")
-  .style("border-radius", "6px")
-  .style("text-align", "center")
-  .style("font-family", "monospace")
-  .style("width", "400px")
-  .text("");
-
-var node = svg
-  .selectAll("circle")
-  .data(data)
-  .enter()
-  .append("circle")
-  .attr("r", function(d) {
-    return scaleRadius(+d.pomodoros);
-  })
-  .style("fill", function(d) {
-    return colorCircles(+d.userId);
-  })
-  .attr("transform", "translate(" + [width / 2, height / 2] + ")")
-  .on("mouseover", function(d) {
-    tooltip.html(d.userId + "<br>" + d.username + "<br>" + d.pomodoros);
-    return tooltip.style("visibility", "visible");
-  })
-  .on("mousemove", function() {
-    return tooltip
-      .style("top", d3.event.pageY - 10 + "px")
-      .style("left", d3.event.pageX + 10 + "px");
-  })
-  .on("mouseout", function() {
-    return tooltip.style("visibility", "hidden");
-  });
-
-function ticked(e) {
-  node
-    .attr("cx", function(d) {
-      return d.x;
-    })
-    .attr("cy", function(d) {
-      return d.y;
-    }); //node is each circle of the bubble chart
-}
+canvas
+  .append("path")
+  .attr("fill", "none")
+  .attr("stroke", "black")
+  .attr("d", diagonal);
--- app/assets/scripts/bubble_chart.js
+++ app/assets/scripts/bubble_chart.js
@@ -1,123 +1,137 @@
-function bubbleChart() {
-  var width = 960,
-    height = 960,
-    maxRadius = 6,
-    columnForColors = "category",
-    columnForRadius = "views";
+// d3.csv("./assets/scripts/medium_january.csv", function(error, data) {
+//   if (error) {
+//     console.error("Error getting or parsing the data.");
+//     throw error;
+//   }
+//   var chart = bubbleChart()
+//     .width(600)
+//     .height(400);
+//   d3
+//     .select("#chart")
+//     .data(data)
+//     .call(chart);
+// });
 
-  function chart(selection) {
-    var data = selection.enter().data();
-    var div = selection,
-      svg = div.selectAll("svg");
-    svg.attr("width", width).attr("height", height);
+// function bubbleChart() {
+//   var width = 960,
+//     height = 960,
+//     maxRadius = 6,
+//     columnForColors = "category",
+//     columnForRadius = "views";
 
-    var tooltip = selection
-      .append("div")
-      .style("position", "absolute")
-      .style("visibility", "hidden")
-      .style("color", "white")
-      .style("padding", "8px")
-      .style("background-color", "#626D71")
-      .style("border-radius", "6px")
-      .style("text-align", "center")
-      .style("font-family", "monospace")
-      .style("width", "400px")
-      .text("");
+//   function chart(selection) {
+//     var data = selection.enter().data();
+//     var div = selection,
+//       svg = div.selectAll("svg");
+//     svg.attr("width", width).attr("height", height);
 
-    var simulation = d3
-      .forceSimulation(data)
-      .force("charge", d3.forceManyBody().strength([-50]))
-      .force("x", d3.forceX())
-      .force("y", d3.forceY())
-      .on("tick", ticked);
+//     var tooltip = selection
+//       .append("div")
+//       .style("position", "absolute")
+//       .style("visibility", "hidden")
+//       .style("color", "white")
+//       .style("padding", "8px")
+//       .style("background-color", "#626D71")
+//       .style("border-radius", "6px")
+//       .style("text-align", "center")
+//       .style("font-family", "monospace")
+//       .style("width", "400px")
+//       .text("");
 
-    function ticked(e) {
-      node
-        .attr("cx", function(d) {
-          return d.x;
-        })
-        .attr("cy", function(d) {
-          return d.y;
-        });
-    }
+//     var simulation = d3
+//       .forceSimulation(data)
+//       .force("charge", d3.forceManyBody().strength([-50]))
+//       .force("x", d3.forceX())
+//       .force("y", d3.forceY())
+//       .on("tick", ticked);
 
-    var colorCircles = d3.scaleOrdinal(d3.schemeCategory10);
-    var scaleRadius = d3
-      .scaleLinear()
-      .domain([
-        d3.min(data, function(d) {
-          return +d[columnForRadius];
-        }),
-        d3.max(data, function(d) {
-          return +d[columnForRadius];
-        })
-      ])
-      .range([5, 18]);
+//     function ticked(e) {
+//       node
+//         .attr("cx", function(d) {
+//           return d.x;
+//         })
+//         .attr("cy", function(d) {
+//           return d.y;
+//         });
+//     }
 
-    var node = svg
-      .selectAll("circle")
-      .data(data)
-      .enter()
-      .append("circle")
-      .attr("r", function(d) {
-        return scaleRadius(d[columnForRadius]);
-      })
-      .style("fill", function(d) {
-        return colorCircles(d[columnForColors]);
-      })
-      .attr("transform", "translate(" + [width / 2, height / 2] + ")")
-      .on("mouseover", function(d) {
-        tooltip.html(
-          d[columnForColors] +
-            "<br>" +
-            d.title +
-            "<br>" +
-            d[columnForRadius] +
-            " hearts"
-        );
-        return tooltip.style("visibility", "visible");
-      })
-      .on("mousemove", function() {
-        return tooltip
-          .style("top", d3.event.pageY - 10 + "px")
-          .style("left", d3.event.pageX + 10 + "px");
-      })
-      .on("mouseout", function() {
-        return tooltip.style("visibility", "hidden");
-      });
-  }
+//     var colorCircles = d3.scaleOrdinal(d3.schemeCategory10);
+//     var scaleRadius = d3
+//       .scaleLinear()
+//       .domain([
+//         d3.min(data, function(d) {
+//           return +d[columnForRadius];
+//         }),
+//         d3.max(data, function(d) {
+//           return +d[columnForRadius];
+//         })
+//       ])
+//       .range([5, 18]);
 
-  chart.width = function(value) {
-    if (!arguments.length) {
-      return width;
-    }
-    width = value;
-    return chart;
-  };
+//     var node = svg
+//       .selectAll("circle")
+//       .data(data)
+//       .enter()
+//       .append("circle")
+//       .attr("r", function(d) {
+//         return scaleRadius(d[columnForRadius]);
+//       })
+//       .style("fill", function(d) {
+//         return colorCircles(d[columnForColors]);
+//       })
+//       .attr("transform", "translate(" + [width / 2, height / 2] + ")")
+//       .on("mouseover", function(d) {
+//         tooltip.html(
+//           d[columnForColors] +
+//             "<br>" +
+//             d.title +
+//             "<br>" +
+//             d[columnForRadius] +
+//             " hearts"
+//         );
+//         return tooltip.style("visibility", "visible");
+//       })
+//       .on("mousemove", function() {
+//         return tooltip
+//           .style("top", d3.event.pageY - 10 + "px")
+//           .style("left", d3.event.pageX + 10 + "px");
+//       })
+//       .on("mouseout", function() {
+//         return tooltip.style("visibility", "hidden");
+//       });
+//   }
 
-  chart.height = function(value) {
-    if (!arguments.length) {
-      return height;
-    }
-    height = value;
-    return chart;
-  };
+//   chart.width = function(value) {
+//     if (!arguments.length) {
+//       return width;
+//     }
+//     width = value;
+//     return chart;
+//   };
 
-  chart.columnForColors = function(value) {
-    if (!arguments.columnForColors) {
-      return columnForColors;
-    }
-    columnForColors = value;
-    return chart;
-  };
+//   chart.height = function(value) {
+//     if (!arguments.length) {
+//       return height;
+//     }
+//     height = value;
+//     return chart;
+//   };
 
-  chart.columnForRadius = function(value) {
-    if (!arguments.columnForRadius) {
-      return columnForRadius;
-    }
-    columnForRadius = value;
-    return chart;
-  };
+//   chart.columnForColors = function(value) {
+//     if (!arguments.columnForColors) {
+//       return columnForColors;
+//     }
+//     columnForColors = value;
+//     return chart;
+//   };
 
-  return chart;
-}
+//   chart.columnForRadius = function(value) {
+//     if (!arguments.columnForRadius) {
+//       return columnForRadius;
+//     }
+//     columnForRadius = value;
+//     return chart;
+//   };
+
+//   return chart;
+// }
